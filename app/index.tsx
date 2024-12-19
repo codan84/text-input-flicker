26ee2ff26923eb42d82a8076088d5fdeb6b84a64
Daniel Gruszczyk <daniel.gruszczyk@live.com>
2024-12-19 22:59:34 +0000

    Added dialog example

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,16 @@
 import { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { StyleSheet } from 'react-native';
-import { Portal, Modal, TextInput, Button, Text } from 'react-native-paper';
+import { Portal, Modal, TextInput, Button, Text, Dialog } from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { PaperProvider } from 'react-native-paper';
 
 export default function HomeScreen() {
   const [ showModal, setShowModal ] = useState(false)
+  const [ showDialog, setShowDialog ] = useState(false)
   const [ text, setText ] = useState('')
   const [ modalText, setModalText ] = useState('')
+  const [ dialogText, setDialogText ] = useState('')
 
   return (
     <SafeAreaProvider>
@@ -16,7 +18,8 @@ export default function HomeScreen() {
         <SafeAreaView>
           <Text>This input works fine:</Text>
           <TextInput value={text} onChangeText={setText} />
-          <Button onPress={() => setShowModal(true)}>Show modal</Button>
+          <Button mode='outlined' onPress={() => setShowModal(true)}>Show modal</Button>
+          <Button mode='outlined' onPress={() => setShowDialog(true)}>Show dialog</Button>
         </SafeAreaView>
         <Portal>
           <Modal
@@ -26,6 +29,12 @@ export default function HomeScreen() {
             <Text>This input flickers:</Text>
             <TextInput value={modalText} onChangeText={setModalText} />
           </Modal>
+          <Dialog visible={showDialog} onDismiss={() => setShowDialog(false)}>
+            <Dialog.Content>
+              <Text>This input flickers:</Text>
+              <TextInput value={dialogText} onChangeText={setDialogText} />
+            </Dialog.Content>
+          </Dialog>
         </Portal>
       </PaperProvider>
     </SafeAreaProvider>
